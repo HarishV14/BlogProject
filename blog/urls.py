2edb901e229f4044c5669f1c48be0498cce31686
HarishV14 <harishv@testpress.in>
2024-09-16 18:48:24 +0530

    Done Sending Email and Comment Section were added till this commit

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- # post views
-    #function based
-    path('', views.post_list, name='post_list'),
-    #class based view
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
+    # post views
+    # function based
+    path("", views.post_list, name="post_list"),
+    # class based view
+    path("", views.PostListView.as_view(), name="post_list"),
+    path(
+        "<int:year>/<int:month>/<int:day>/<slug:post>/",
+        views.post_detail,
+        name="post_detail",
+    ),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
 app_name = 'blog'
